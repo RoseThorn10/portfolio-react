f9804347faa1b7fcc22782ade25ba8a8da668890
RoseThorn10 <lrroman@comcast.net>
2022-11-01 17:52:27 -0500

    Adds navbar links and styling

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a className="site-title" href="/">Leah Roman</a>
+        <ul>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/projects">My Work</a>
+            </li>
+            <li>
+                <a href="/contact">Contact Me</a>
+            </li>
+        </ul>
+    </nav>
+}
