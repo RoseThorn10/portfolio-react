faee3fa7146347652e896f460b0946e63c7c5410
RoseThorn10 <lrroman@comcast.net>
2022-11-01 22:01:10 -0500

    Adds footer

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+         I'm a web developer living in the Chicago area. I love to learn and I'm always eager to learn new skills. My top-notch communication skills help me get to know my clients and find out what they really want and need from a project. From there, I take great pride in bringing their dreams to life!
+        </p>
+      </div>
+    );
+  }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,16 @@ export default function Navbar() {
         <a className="site-title" href="/">Leah Roman</a>
         <ul>
             <li>
-                <a href="/about">About</a>
+                <a href="/about" className="active">About</a>
             </li>
             <li>
                 <a href="/projects">My Work</a>
             </li>
             <li>
                 <a href="/contact">Contact Me</a>
+            </li>
+            <li>
+                <a href="/resume">Resume</a>
             </li>
         </ul>
     </nav>
