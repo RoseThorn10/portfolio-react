a609e8d0ba114b3edb96c15dc07c5f6723490322
RoseThorn10 <lrroman@comcast.net>
2022-11-03 21:29:40 -0500

    Adds images and CSS on project cards

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ export default function About() {
     return (
       <div>
         <h2>About Me</h2>
-        <p>
+        <p className="about">
          I'm a web developer living in the Chicago area. I'm always eager to learn new skills. I enjoy working with my team and clients to build something to be proud of.
         </p>
-        <p>
+        <p className="about">
           My skills include HTML/CSS, Javascript, SQL and NOSQL databases, and a few others.
         </p>
       </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Card from 'react-bootstrap/Card';
 import pwdimg from '../../Images/pwdgen.png';
+import wthrimg from '../../Images/weather-page.png';
+import AAIC from '../../Images/AAIC.png';
+import joy from '../../Images/joypainting.png';
 
 
 export default function Projects() {
@@ -8,37 +11,37 @@ export default function Projects() {
         <div>
             <h2>My Work</h2>
             <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
-                <Card style={{ maxWidth: '18rem' }}>
+                <Card style={{ width: '18rem' }}>
                     <Card.Img src={pwdimg} variant="top" alt="a password generator application" />
                     <Card.Body>
                         <Card.Text className="card-text">A password generator that can apply a number of different parameters.</Card.Text>
                         <Card.Link href="https://rosethorn10.github.io/password-gen/" target="_blank">Password Generator</Card.Link>
                     </Card.Body>
                 </Card>
+                {/* </div> */}
+                <Card style={{ maxWidth: '18rem' }}>
+                    <Card.Img src={wthrimg} variant="top" alt="weather application" />
+                    <Card.Body>
+                        <Card.Text class="card-text">A website which uses a data API to update weather conditions and a forecast.</Card.Text>
+                        <Card.Link href="https://rosethorn10.github.io/weather-dashboard/" target="_blank" class="card-link">Weather Dashboard</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style={{ maxWidth: '18rem' }}>
+                    <Card.Img src={AAIC} variant="top" alt="the painting American Gothic" />
+                    <Card.Body>
+                        <Card.Text class="card-text">A group project demonstrating a variety of front-end web development skills.</Card.Text>
+                        <Card.Link href="https://marianellag1.github.io/Explore-Art-Chicago/" target="_blank">Explore Art Chicago</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style={{ maxWidth: '18rem' }}>
+                    <Card.Img src={joy} variant="top" alt="Joy of Painting theme" />
+                    <Card.Body>
+                        <Card.Text class="card-text">A group project building a complete full-stack web application</Card.Text>
+                        <Card.Link href="https://warm-castle-73528.herokuapp.com/" target="_blank">Happy Little Trees</Card.Link>
+                    </Card.Body>
+                </Card>
             </div>
-            {/* <div class="card" style="width: 18rem;">
-                <img src="./assets/images/weather-page.png" class="card-img-top"
-                    alt="a weather dashboard web application" />
-                <div class="card-body">
-                    <p class="card-text">A website which uses a data API to update weather conditions and a forecast.</p>
-                    <Card.Link href="https://rosethorn10.github.io/weather-dashboard/" target="_blank" class="card-link">Weather Dashboard</Card.Link>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="./assets/images/AAIC.png" alt="the painting American Gothic on a web site" />
-                <div class="card-body">
-                    <p class="card-text">A group project demonstrating a variety of front-end web development skills.</p>
-                    <a href="https://marianellag1.github.io/Explore-Art-Chicago/" target="_blank">Explore Art Chicago</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="./assets/images/joypainting.png" alt="opening theme from Joy of Painting" />
-                <div class="card-body">
-                    <p class="card-text">A group project building a complete full-stack web application</p>
-                    <a href="https://warm-castle-73528.herokuapp.com/" target="_blank">Happy Little Trees</a>
-                </div>
-            </div> 
-*/}
-        </div>
-    )
+        </div>    
+    )    
+            
 }
