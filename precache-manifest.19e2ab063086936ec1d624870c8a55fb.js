ca6c67dbc5f8b7345497481f814470bf8853d927
RoseThorn10 <lrroman@comcast.net>
2022-11-07 09:48:08 -0600

    Updates

---
+++ precache-manifest.19e2ab063086936ec1d624870c8a55fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5919a267d50595ef243f",
+    "url": "/portfolio-leah/static/css/main.0dee41f6.chunk.css"
+  },
+  {
+    "revision": "5919a267d50595ef243f",
+    "url": "/portfolio-leah/static/js/main.5919a267.chunk.js"
+  },
+  {
+    "revision": "349b88b988cb4397a859",
+    "url": "/portfolio-leah/static/css/1.a6f297ed.chunk.css"
+  },
+  {
+    "revision": "349b88b988cb4397a859",
+    "url": "/portfolio-leah/static/js/1.349b88b9.chunk.js"
+  },
+  {
+    "revision": "5251601bca0f3e9c5476",
+    "url": "/portfolio-leah/static/js/runtime~main.5251601b.js"
+  },
+  {
+    "revision": "a1b89608f3be6922fb99592fd42f2919",
+    "url": "/portfolio-leah/static/media/Chicago_skyline_kz03.a1b89608.jpg"
+  },
+  {
+    "revision": "90d1220149213a3c7d35ffa9cf476bc7",
+    "url": "/portfolio-leah/static/media/weather-page.90d12201.png"
+  },
+  {
+    "revision": "a9de5776a1ca6c97ca00ea00d80078d4",
+    "url": "/portfolio-leah/static/media/AAIC.a9de5776.png"
+  },
+  {
+    "revision": "855658f0ccb1e9789a7e97b6ce65a43d",
+    "url": "/portfolio-leah/static/media/joypainting.855658f0.png"
+  },
+  {
+    "revision": "58804ab5e98ae93c022f5cd58ec7ff94",
+    "url": "/portfolio-leah/index.html"
+  }
+];
